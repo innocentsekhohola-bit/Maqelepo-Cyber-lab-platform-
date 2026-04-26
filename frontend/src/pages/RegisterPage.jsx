e6376aaf4724e3c36cab5b23216ec644d6473bb7
innocentsekhohola-bit <innocentsekhohola@gmail.com>
2026-04-26 05:37:44 +0200

    Update RegisterPage.jsx

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "./Auth.css";
 
-const API = "http://localhost:5000/api";
+const API = "https://maqelepo-cyber-lab-platform-production.up.railway.app/api";
 
 export default function RegisterPage() {
   const [form, setForm] = useState({ username: "", email: "", password: "" });
