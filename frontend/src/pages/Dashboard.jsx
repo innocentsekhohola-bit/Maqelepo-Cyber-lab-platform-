042ccb1b653f75b3574688356612b63750f11881
innocentsekhohola-bit <innocentsekhohola@gmail.com>
2026-04-26 08:23:46 +0200

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Dashboard.css";
 
-const API = "https://maqelepo-cyber-lab-platform-production.up.railway.app/api";
+const API = "https://maqelepo.pythonanywhere.com/api";
 const RANK_COLORS = {
   "Novice": "#6b7280",
   "Script Kiddie": "#10b981",
